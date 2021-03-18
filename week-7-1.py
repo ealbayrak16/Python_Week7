b716db1f37f9c90788c8a6370fabedd844e75f20
ealbayrak16 <78041118+ealbayrak16@users.noreply.github.com>
2021-03-18 18:01:10 +0200

    Delete week-7-1.py

--- week-7-1.py
+++
@@ -1,9 +0,0 @@
-"""
-Input : AABZA1111AEGTV5YH678MK4FM53B6
-Output : MK4FM53B6
-Input : AEGTV5VZ4PF94B6YH678
-Output : VZ4PF94B6"""
-import regex as re
-text_ = "AEGTV5VZ4PF94B6YH678 AABZA1111AEGTV5YH678MK4FM53B6"
-x = re.findall("[(A-Z)|(a-z)]{2}\d[(A-Z)|(a-z)]{2}\d{2}[(A-Z)|(a-z)]\d",text_)
-print(" ".join(x))
